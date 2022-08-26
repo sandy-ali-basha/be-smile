e4fe6303635f61817fc6af8b7e6fa6716582a754
sandy <sandy.ali.basha@gmail.com>
2022-08-26 16:39:13 +0300

    first

---
+++ assets/js/swiper.config.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".aboutUs_Swiper", {
+    spaceBetween: 30,
+    loop: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+
+    navigation: {
+        nextEl: ".swiper_button_next",
+        prevEl: ".swiper_button_prev",
+    },
+});
