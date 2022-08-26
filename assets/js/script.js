695254adee247ee6b6c06b5423b2535139553764
sandy <sandy.ali.basha@gmail.com>
2022-08-26 20:12:06 +0300

    animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
     $(".loader").hide();
+    for (let i = 1; i < 30; i++) {
+        $(".dotts").append(
+            `<div class="dot">`
+        );
+    }
+    $(".arabic").click(() => {
+        $("body").addClass("rtl")
+        document.body.setAttribute('dir', 'rtl')
+    })
+    $(".english").click(() => {
+        $("body").removeClass("rtl")
+        document.body.setAttribute('dir', 'ltr')
+    })
+    $(".viewMore_btn").click(() => {
+        $(".card_container").removeClass("d-none")
+        $(".card_container").addClass("animate__fadeIn")
+        $(".viewMore_btn").addClass("d-none")
+
+    })
+
 });
